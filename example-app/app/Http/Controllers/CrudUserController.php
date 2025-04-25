7db0214a14b25b3e856334b801a9be1ec8b109bb
Dong dep trai <dong12062004@gmail.com>
2025-04-25 16:22:42 +0700

    themorders

---
+++ example-app/app/Http/Controllers/CrudUserController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Contracts\Session\Session as SessionSession;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash as FacadesHash;
+use Illuminate\Support\Facades\Session as FacadesSession;
+
+/**
+ * CRUD User controller
+ */
+class CrudUserController extends Controller
+{
+
+    /**
+     * Login page
+     */
+    public function login()
+    {
+        return view('exe.exe1.login');
+    }
+    public function index()
+    {
+        return view('exe.exe1.index');
+    }
+
+    /**
+     * User submit form login
+     */
+    public function authUser(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {  
+            // dd(Auth::check());
+
+            return redirect()->intended('list')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    /**
+     * Registration page
+     */
+    public function register()
+    {
+        return view('exe/exe1/register');
+    }
+
+    /**
+     * User submit form register
+     */
+    public function postUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'confirm-password' =>'required|same:password',
+        ]);
+
+        $data = $request->all();
+        $check = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => FacadesHash::make($data['password'])
+        ]);
+
+        return redirect("login");
+    }
+
+    /**
+     * View user detail page
+     */
+    public function readUser(Request $request) {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('exe.exe1.view', ['messi' => $user]);
+    }
+
+    /**
+     * Delete user by id
+     */
+    public function deleteUser(Request $request) {
+        $user_id = $request->get('id');
+        $user = User::destroy($user_id);
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * Form update user page
+     */
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('exe.exe1.update', ['user' => $user]);
+    }
+
+    /**
+     * Submit form update user
+     */
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,id,'.$input['id'],
+            'password' => 'required|min:6',
+            'confirm-password' =>'required|same:password',
+        ]);
+
+       $user = User::find($input['id']);
+       $user->name = $input['name'];
+       $user->email = $input['email'];
+       $user->password = $input['password'];
+       $user->save();
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * List of users
+     */
+    public function listUser()
+    {
+        if(Auth::check()){
+            $users = User::all();
+            return view('exe.exe1.list', ['users' => $users]);
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
+    /**
+     * Sign out
+     */
+    public function signOut() {
+        FacadesSession::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
---
+++ example-app/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\CrudUserController;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('dashboard', [CrudUserController::class, 'dashboard']);
+
+Route::get('login', [CrudUserController::class, 'login'])->name('login');
+Route::post('login', [CrudUserController::class, 'authUser'])->name('user.authUser');
+Route::get('signout', [CrudUserController::class, 'login'])->name('signout');
+Route::get('index', [CrudUserController::class, 'index'])->name('index');
+Route::get('register', [CrudUserController::class, 'register'])->name('register');
+Route::post('register', [CrudUserController::class, 'postUser'])->name('user.postUser');
+// Route::get('list', [CrudUserController::class, 'listUser'])->name('list');
+Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
+Route::get('update', [CrudUserController::class, 'updateUser'])->name('user.updateUser');
+Route::get('delete', [CrudUserController::class, 'deleteUser'])->name('user.deleteUser');
+Route::get('read', [CrudUserController::class, 'readUser'])->name('user.readUser');
+Route::post('postUpdateUser', [CrudUserController::class, 'postUpdateUser'])->name('user.postUpdateUser');
+
+Route::get('/',function(){
+return view("welcome");
+});
